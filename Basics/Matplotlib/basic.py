140750701ce502f4c691b01978f5323a81efe8a6
huzaifa003 <buntyhuzaifa3@gmail.com>
2022-09-26 02:38:35 +0500

    Added basics of matplotlib

---
+++ Basics/Matplotlib/basic.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+from matplotlib import style;
+
+#Styling
+print(style.available)
+style.use('dark_background')
+
+#Coordinates
+x1 = [2,3,4]
+y1 = [5,6,7]
+
+x2 = [6,7,8]
+y2 = [7,10,12]
+
+#Plotting of Coordinates
+plt.plot(x1,y1)
+plt.plot(x2,y2)
+
+#Lables and titles 
+plt.title('testing')
+plt.xlabel('X Labels')
+plt.ylabel('Y Label')
+plt.show()
+
