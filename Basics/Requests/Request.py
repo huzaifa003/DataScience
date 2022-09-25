21830bbd057ccb564cc5dbae48671cf73704bcc3
huzaifa003 <buntyhuzaifa3@gmail.com>
2022-09-25 18:17:41 +0500

    Added Basic Types

--- Basics/Requests/Request.py
+++ Basics/Requests/Request.py
@@ -1,7 +1,7 @@
 import json
 import requests
 import tokens 
-x = requests.get('https://api.github.com/users',params=tokens.token) #Api Key Token (Replace tokens.token with your own access token)
+x = requests.get('https://api.github.com/users',params=tokens.token) #Api Key Token (Replace tokens.token with your own access token <params = 'xyzadbz024bzs'> and delete import tokens)
 
 print(x.status_code)
 print(x.json()) #Converts the Reponse object into a json object
---
+++ Basics/dataStructs.py
@@ -0,0 +1,16 @@
+print("\n---------------DICTIONARY--------------------")
+dictionary1 = {'name' : 'xyz', 'grade' : 2} #Key Pair values Same as JSON
+print(dictionary1['name'])
+print(dictionary1['grade'])
+print(dictionary1.keys())
+print(dictionary1.values())
+
+print("\n---------------ARRAY--------------------")
+arr1 = [1,2,3,4] #Simple arrays More in numpy
+print(arr1[0])
+print(arr1[2])
+print (arr1)
+
+print("\n---------------TUPPLE--------------------")
+tupple1 = (1,2,3)  #Immutable, Cant be changed
+print(tupple1)
